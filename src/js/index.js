648d34a673a3c4cee8d406b3ba062f97399d96d8
Daniil <aikis11041996@gmail.com>
2024-06-11 21:49:10 +0300

    last before change

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import '../scss/style.scss'
 
 // Инициализируем Swiper
 
-var swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
